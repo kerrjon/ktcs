3692f83104b01e82a328ff30d5a8ed13efb92fad
Kerr <kerrjon@yahoo.com>
2016-06-27 23:02:35 -0500

    intial commit

---
+++ Ktcs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ktcs.Startup))]
+namespace Ktcs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
