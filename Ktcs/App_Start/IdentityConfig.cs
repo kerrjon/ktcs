a8e39f091b7fd497f3bf26b18df08bda70b8a1fa
Kerr <kerrjon@yahoo.com>
2016-07-07 23:28:13 -0500

    adding KTCS Db Context

--- Ktcs/App_Start/IdentityConfig.cs
+++ Ktcs/App_Start/IdentityConfig.cs
@@ -14,79 +14,79 @@ using Ktcs.Areas.Login.Models;
 
 namespace Ktcs
 {
-    public class EmailService : IIdentityMessageService
+  public class EmailService : IIdentityMessageService
+  {
+    public Task SendAsync(IdentityMessage message)
     {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
-        }
+      // Plug in your email service here to send an email.
+      return Task.FromResult(0);
+    }
+  }
+
+  public class SmsService : IIdentityMessageService
+  {
+    public Task SendAsync(IdentityMessage message)
+    {
+      // Plug in your SMS service here to send a text message.
+      return Task.FromResult(0);
+    }
+  }
+
+  // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+  public class ApplicationUserManager : UserManager<ApplicationUser>
+  {
+    public ApplicationUserManager(IUserStore<ApplicationUser> store)
+        : base(store)
+    {
     }
 
-    public class SmsService : IIdentityMessageService
+    public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
     {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
-        }
+      var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+      // Configure validation logic for usernames
+      manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+      {
+        AllowOnlyAlphanumericUserNames = false,
+        RequireUniqueEmail = true
+      };
+
+      // Configure validation logic for passwords
+      manager.PasswordValidator = new PasswordValidator
+      {
+        RequiredLength = 6,
+        RequireNonLetterOrDigit = false,
+        RequireDigit = false,
+        RequireLowercase = false,
+        RequireUppercase = false,
+      };
+
+      // Configure user lockout defaults
+      manager.UserLockoutEnabledByDefault = true;
+      manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+      manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+      // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+      // You can write your own provider and plug it in here.
+      manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
+      {
+        MessageFormat = "Your security code is {0}"
+      });
+      manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
+      {
+        Subject = "Security Code",
+        BodyFormat = "Your security code is {0}"
+      });
+      manager.EmailService = new EmailService();
+      manager.SmsService = new SmsService();
+      var dataProtectionProvider = options.DataProtectionProvider;
+      if (dataProtectionProvider != null)
+      {
+        manager.UserTokenProvider =
+            new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+      }
+      return manager;
     }
-
-    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-    public class ApplicationUserManager : UserManager<ApplicationUser>
-    {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
-            : base(store)
-        {
-        }
-
-        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
-        {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
-            // Configure validation logic for usernames
-            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
-            {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
-            };
-
-            // Configure validation logic for passwords
-            manager.PasswordValidator = new PasswordValidator
-            {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = false,
-                RequireDigit = false,
-                RequireLowercase = false,
-                RequireUppercase = false,
-            };
-
-            // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
-            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
-
-            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
-            // You can write your own provider and plug it in here.
-            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
-            {
-                MessageFormat = "Your security code is {0}"
-            });
-            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
-            {
-                Subject = "Security Code",
-                BodyFormat = "Your security code is {0}"
-            });
-            manager.EmailService = new EmailService();
-            manager.SmsService = new SmsService();
-            var dataProtectionProvider = options.DataProtectionProvider;
-            if (dataProtectionProvider != null)
-            {
-                manager.UserTokenProvider = 
-                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
-            }
-            return manager;
-        }
-    }
+  }
 
 
   // Configure the RoleManager used in the application. RoleManager is defined in the ASP.NET Identity core assembly
@@ -105,22 +105,22 @@ namespace Ktcs
 
   // Configure the application sign-in manager which is used in this application.
   public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+  {
+    public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+        : base(userManager, authenticationManager)
     {
-        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
-            : base(userManager, authenticationManager)
-        {
-        }
-
-        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
-        {
-            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
-        }
-
-        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
-        {
-            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
-        }
     }
+
+    public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+    {
+      return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+    }
+
+    public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+    {
+      return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+    }
+  }
 
   // This is useful if you do not want to tear down the database each time you run the application.
   // public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
@@ -169,7 +169,7 @@ namespace Ktcs
       }
       catch (Exception exception)
       {
-       // Log.Error("error trying to seed database", exception);
+        // Log.Error("error trying to seed database", exception);
       }
     }
 
---
+++ Ktcs/DAL/KtcsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Ktcs.DAL
+{
+  public class KtcsDbContext : DbContext
+  {
+    public KtcsDbContext() : base("DefaultConnection")
+    {
+    }
+
+    public static KtcsDbContext Create()
+    {
+      return new KtcsDbContext();
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
+  }
+}
